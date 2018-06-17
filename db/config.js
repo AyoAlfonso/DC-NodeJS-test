a756e8383251e0b95b673998403928c039b24f0b
AyoAlfonso <alfonsoayo7@gmail.com>
2018-06-17 08:02:32 +0100

    Moved

--- db/config.js
+++ db/config.js
@@ -6,6 +6,6 @@ let redis = RedisInst.createClient(options)
 
 
 module.exports =  {
-        POSTGRES_URL: "mysql://testuser:6e48a95100f2@nannywaitlistdbtest.cki6wpslsotr.us-east-1.rds.amazonaws.com:3306/nannywaitlistdbtest",
+        POSTGRES_URL: "",
         redis
 }
